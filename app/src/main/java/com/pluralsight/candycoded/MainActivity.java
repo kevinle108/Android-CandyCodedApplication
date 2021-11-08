97ee9e386cf179541654d78768a5a275ef50043c
Kevin Le <kevinle108@gmail.com>
2021-11-08 12:44:52 -0500

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -82,16 +82,15 @@ public class MainActivity extends AppCompatActivity {
     return true;
   }
 
+  // **
+  // TODO - Task 1 - Show Store Information Activity
+  // **
   @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     Intent infoIntent = new Intent(this, InfoActivity.class);
     startActivity(infoIntent);
     return super.onOptionsItemSelected(item);
   }
-
-  // **
-  // TODO - Task 1 - Show Store Information Activity
-  // **
 
   private void addCandiesToDatabase(Candy[] candies) {
     SQLiteDatabase db = candyDbHelper.getWritableDatabase();
